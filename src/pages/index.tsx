4bb15d7b7e6132ddbe4c633fe33804549cf343a2
MaksimMaltsev <m.maltsev@dsec.com>
2023-02-26 16:28:19 +0300

    add ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,14 @@ import Ptag from "@/components/Ptag/Ptag";
 import Tag from "@/components/Tag/Tag";
 import Rating from "@/components/Rating/Rating";
 import {withLayout} from "../../layout/Layout";
+import {GetStaticProps} from "next";
+import axios from "axios";
+import * as process from "process";
+import {MenuItem} from "../../interfaces/menu.interface";
 
-function Home(): JSX.Element {
+function Home({menu} :HomeProps): JSX.Element {
   return (
-    < >
+    <>
         <Htag tag='h1'>Курсы по Photoshop</Htag>
         <Button appearance="primary">Узнать подробнее</Button>
         <Button appearance="ghost" arrow="right">Узнать подробнее</Button>
@@ -16,7 +20,27 @@ function Home(): JSX.Element {
         </Ptag>
         <Tag>Photoshop</Tag>
         <Rating rating={4} isEditable/>
+        <ul>
+            {menu.map(m  => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+        </ul>
     </>
   );
 }
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+    const firstCategory = 0;
+    const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+        firstCategory
+    });
+    return {
+        props : {
+            menu,
+            firstCategory
+        }
+    };
+};
+interface HomeProps extends Record<string, unknown>{
+    menu: MenuItem[];
+    firstCategory: number;
+}
