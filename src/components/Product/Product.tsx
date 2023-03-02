ca23531bb8ae5d77564ebf85ca0d46b2f0917239
maksim <m.maltsev@dsec.ru>
2023-03-02 20:29:24 +0300

    product page done

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -8,14 +8,15 @@ import Rating from "@/components/Rating/Rating";
 import Tag from "@/components/Tag/Tag";
 import * as process from "process";
 import Button from "@/components/Button/Button";
-import {priceRu} from "@/helpers/helpers";
+import {declOfNum, priceRu} from "@/helpers/helpers";
 import Devider from "@/components/Devider/Devider";
+import Image from "next/image";
 
 function Product({product, className, ...props} :ProductProps) :JSX.Element {
     return (
        <Card className={classes.product}>
            <div className={classes.logo}>
-               <img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title}/>
+               <Image src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} width={70} height={70} />
            </div>
            <div className={classes.title}>
                <Htag tag='h3'>{product.title}</Htag>
@@ -31,10 +32,17 @@ function Product({product, className, ...props} :ProductProps) :JSX.Element {
            <div className={classes.tags}>{product.categories.map(c => <Tag key={c} color='ghost' className={classes.category}>{c}</Tag>)}</div>
            <div className={classes.priceTitle}>Цена</div>
            <div className={classes.creditTitle}>Кредит</div>
-           <div className={classes.rateTitle}>{product.reviewCount} отзывов</div>
+           <div className={classes.rateTitle}>{product.reviewCount} {declOfNum(product.reviewCount, ['Отзыв', 'Отзыва', 'Отзывов'])}</div>
            <Devider className={classes.hr} />
            <div className={classes.description}>{product.description}</div>
-           <div className={classes.features}>Features</div>
+           <div className={classes.feature}>
+               {product.characteristics.map((c) =>
+                   (<div className={classes.characteristics} key={c.name}>
+                        <span className={classes.characteristicName}>{c.name}</span>
+                        <span className={classes.characteristicDots}></span>
+                        <span className={classes.characteristicValue}>{c.value}</span>
+               </div>) )}
+           </div>
            <div className={classes.advBlock}>
                {product.advantages && <div className={classes.advantages}>
                    <div className={classes.advTitle}>Преимущества</div>
@@ -45,7 +53,7 @@ function Product({product, className, ...props} :ProductProps) :JSX.Element {
                    <div>{product.disAdvantages}</div>
                </div>}
            </div>
-           <Devider className={classes.hr} />
+           <Devider className={cn(classes.hr, classes.hr2)} />
            <div className={classes.actions}>
                <Button appearance={'primary'}>Узнать подробнее</Button>
                <Button appearance={'ghost'} arrow={'right'} className={classes.reviewBtn}>Читать отзывы</Button>
